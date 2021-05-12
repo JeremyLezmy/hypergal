67d3348c37b2d8feab8775fc273647ac8e52fce0
MickaelRigault <mick.rigault@gmail.com>
2021-05-12 14:36:14 +0200

    cleaning

--- hypergal/spectroscopy/sedmtools.py
+++ hypergal/spectroscopy/sedmtools.py
@@ -1,86 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-################################################################################
-# Filename:          sedmtools.py
-# Description:       script description
-# Author:            Jeremy Lezmy <lezmy@ipnl.in2p3.fr>
-# Author:            $Author: jlezmy $
-# Created on:        $Date: 2021/05/10 17:27:54 $
-# Modified on:       2021/05/10 17:57:20
-# Copyright:         2019, Jeremy Lezmy
-# $Id: sedmtools.py, 2021/05/10 17:27:54  JL $
-################################################################################
 
-"""
-.. _sedmtools.py:
-
-sedmtools.py
-==============
-
-
-"""
-__license__ = "2019, Jeremy Lezmy"
-__docformat__ = 'reStructuredText'
-__author__ = 'Jeremy Lezmy <lezmy@ipnl.in2p3.fr>'
-__date__ = '2021/05/10 17:27:54'
-__adv__ = 'sedmtools.py'
-
-import os
-import sys
-import datetime
-from ztfquery import sedm, io
-from pysedm import astrometry, byecr
-import pysedm
+from ztfquery import sedm
+from pysedm import byecr
 from shapely.geometry.point import Point
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    
+# color printing
+from ..utils.tools import bcolors
 
-SEDMDIROUT = os.path.join(SEDMLOCAL_BASESOURCE,"redux")
-PHAROS_DATALOC = 'http://pharos.caltech.edu/data/'
-
-
-def remove_targ_spx(cube, xy, radius = 3, store = True, get_filename = True):
+def remove_targ_spx(cube, xy, radius=3, store=True, get_filename=True):
     
     """ Remove spaxels around the target position
-        Parameters
-        ----------
-        cube: [pyifu.Cube]
 
-        xy: [list, array 2D]
-            Target Position (spx coordinate)
+    Parameters
+    ----------
+    cube: [pyifu.Cube]
 
-        radius: [float]
-                radius of the circle centered on xy where you want to remove spaxels (spx coordinates)
-                Default is 3.
+    xy: [list, array 2D]
+        Target Position (spx coordinate)
 
-        store: [bool]
-               Do you want to store the new cube?
+    radius: [float]
+        radius of the circle centered on xy where you want to remove spaxels (spx coordinates)
+        Default is 3.
+
+    store: [bool]
+        Do you want to store the new cube?
        
-        get_filename: [bool]
-               If True, return filename of the new cube
-               If False, return the new cube object
+    get_filename: [bool]
+        If True, return filename of the new cube
+        If False, return the new cube object
 
-        Returns
-        -------
-        New cube without spaxels
-        """
-    
+    Returns
+    -------
+    New cube without spaxels
+    """
     x, y = xy
     p = Point(x, y)
     circle = p.buffer(radius)
     idx = cube.get_spaxels_within_polygon( circle )
     cube_wotar =cube.get_partial_cube([i for i in cube.indexes if i not in idx],np.arange(len(cube.lbda)) )
-    cube_wotar.set_filename( cube.filename.replace('e3d', 'e3d_spx_removed') )
+    cube_wotar.set_filename( cube.filename.replace('e3d', 'e3dobjrm') )
     
     if store:
         cube_wotar.writeto( cube_wotar.filename )
@@ -91,63 +50,70 @@ def remove_targ_spx(cube, xy, radius = 3, store = True, get_filename = True):
     return cube_wotar
 
 
+def get_byecr_cube(self, cube , cut_critera = 5, store = True, get_filename = True,
+                       verbose=False):
+    """ Apply byecr method (cosmic ray removing) over sedm cube
 
-def get_byecr_cube(self, cube , cut_critera = 5, store = True, get_filename = True):
+     Parameters
+     ----------
+     cube: [pyifu.Cube]
+     
+     cut_criteria: [float] -optional-
+        cut criteria values we want to use.
+        Default is '5'.
 
-        """ Apply byecr method (cosmic ray removing) over sedm cube
-
-        Parameters
-        ----------
-        cube: [pyifu.Cube]
-
-        cut_criteria: [float] -optional-
-            cut criteria values we want to use.
-            Default is '5'.
-
-        store: [bool]
-               Do you want to store the new cube?
+    store: [bool]
+        Do you want to store the new cube?
        
-        get_filename: [bool]
-               If True, return filename of the new cube
-               If False, return the new cube object
+    get_filename: [bool]
+        If True, return filename of the new cube
+        If False, return the new cube object
                 
-        Returns
-        -------
-        New cube without cosmic ray
-        """
+    Returns
+    -------
+    Cube
+    """
 
-        night = cube.header['OBSDATE'].rsplit('-')
-        night = ''.join(night)
+    night = cube.header['OBSDATE'].rsplit('-')
+    night = ''.join(night)
 
-        try:
-            bycrcl = byecr.SEDM_BYECR( night, cube)
-            cr_df = bycrcl.get_cr_spaxel_info(None, False, cut_critera)
+    # We should avoid the try exect.
+    # TODO: squery.get_hexagrid(date)
+    try:
+        bycrcl = byecr.SEDM_BYECR( night, cube)
+        cr_df = bycrcl.get_cr_spaxel_info(None, False, cut_critera)
             
-        except:
-            download_hexagrid(night)
-            bycrcl = pysedm.byecr.SEDM_BYECR( night, cube)
-            cr_df = bycrcl.get_cr_spaxel_info(None, False, cut_critera)
+    except:
+        download_hexagrid(night)
+        bycrcl = byecr.SEDM_BYECR( night, cube)
+        cr_df = bycrcl.get_cr_spaxel_info(None, False, cut_critera)
 
+    if verbose:
         print( bcolors.OKBLUE + f" Byecr succeeded, {len(cr_df)} detected cosmic-rays removed!" + bcolors.ENDC)
 
-        cube_bycr = cube.copy()
-        cube_bycr.data[cr_df["cr_lbda_index"], cr_df["cr_spaxel_index"]] = np.nan
-        cube_bycr.header.set("NCR", len(cr_df), "total number of detected cosmic-rays from byecr")
-        cube_bycr.header.set("NCRSPX", len(np.unique(cr_df["cr_spaxel_index"])), "total number of cosmic-ray affected spaxels")
+    cube_bycr = cube.copy()
+    cube_bycr.data[cr_df["cr_lbda_index"], cr_df["cr_spaxel_index"]] = np.nan
+    cube_bycr.header.set("NCR", len(cr_df), "total number of detected cosmic-rays from byecr")
+    cube_bycr.header.set("NCRSPX", len(np.unique(cr_df["cr_spaxel_index"])), "total number of cosmic-ray affected spaxels")
 
-        cube_bycr.set_filename( cube.filename.replace('e3d', 'e3d_crr') )
+    cube_bycr.set_filename( cube.filename.replace('e3d_crr', 'e3d_bycr') )
         
-        if store:
-            cube_bycr.writeto( cube_bycr.filename)
+    if store:
+        cube_bycr.writeto( cube_bycr.filename)
 
-        if get_filename:
-            return cube_byecr.filename
+    if get_filename:
+        return cube_byecr.filename
              
-        return cube_bycr
+    return cube_bycr
 
 
 def download_hexagrid(self, night, dirout = SEDMDIROUT, nodl = False, show_progress = False, overwrite=False):
-        
-    io.download_single_url( url = os.path.join(PHAROS_DATALOC, night, night +'_HexaGrid.pkl'), fileout =os.path.join( dirout, night, night + '_HexaGrid.pkl'), show_progress=False, overwrite=overwrite )
-
-# End of sedmtools.py ========================================================
+    """ """
+    # THIS shoudl use ZTFquery
+    SEDMDIROUT = os.path.join(SEDMLOCAL_BASESOURCE,"redux")
+    PHAROS_DATALOC = 'http://pharos.caltech.edu/data/'
+    import os
+    from ztfquery import io
+    io.download_single_url( url = os.path.join(PHAROS_DATALOC, night, night +'_HexaGrid.pkl'),
+                            fileout =os.path.join( dirout, night, night + '_HexaGrid.pkl'),
+                            show_progress=False, overwrite=overwrite )
--- hypergal/utils/tools.py
+++ hypergal/utils/tools.py
@@ -1,5 +1,17 @@
 
 import numpy as np
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'    
+
 def parse_vmin_vmax(data, vmin, vmax):
     """ """
     if vmax is None: vmax="99"
